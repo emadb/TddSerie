169c2a4dda1f9c91c72deffb331ae0d2032b51b3
Emanuele DelBono <emanuele@codiceplastico.com>
2012-02-19 21:59:55 +0100

    created project

---
+++ rakefile.rb
@@ -0,0 +1,36 @@
+require 'rake/clean'
+
+# prject variables
+DOT_NET_PATH = "#{ENV["SystemRoot"]}\\Microsoft.NET\\Framework\\v4.0.30319"
+XUNIT_EXE = "xunit.console.clr4.exe"
+SOURCE_PATH = "src"
+OUTPUT_PATH = "output"
+CONFIG = "Release"
+DEPLOY_PATH = "./archive"
+ 
+CLEAN.include(OUTPUT_PATH)
+
+# Tasks
+task :default => [:nuget, :compile, :unit_test]
+
+desc "Install packages with nuget"
+task :nuget do
+    Dir.glob("#{SOURCE_PATH}/**/packages.config").each do |f|
+        sh "nuget.exe install #{f} -OutputDirectory #{SOURCE_PATH}/packages"
+    end 
+end
+
+desc "Build solutions using MSBuild"
+task :compile  do  
+  solutions = FileList["#{SOURCE_PATH}/**/*.sln"]
+  solutions.each do |solution|
+    sh "#{DOT_NET_PATH}/msbuild.exe /p:Configuration=#{CONFIG} #{solution}"
+  end
+end
+   
+desc "Running unit tests"
+task :unit_test do
+  Dir.glob("#{SOURCE_PATH}/*.Fixture/bin/#{CONFIG}/*.Fixture.dll").each do |f| 
+    sh "#{XUNIT_EXE} #{f}"
+  end 
+end
