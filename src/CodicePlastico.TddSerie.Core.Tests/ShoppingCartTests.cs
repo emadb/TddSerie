ec3286c8b3e9be2d18f6b58763461d09f2571bd7
emadb <emanuele@codiceplastico.com>
2012-03-12 23:41:11 +0100

    first tye, added two tests for PriceList

--- src/CodicePlastico.TddSerie.Core.Tests/ShoppingCartTests.cs
+++ src/CodicePlastico.TddSerie.Core.Tests/ShoppingCartTests.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using Xunit;
+using Moq;
 
 namespace CodicePlastico.TddSerie.Core.Tests
 {
@@ -33,6 +34,32 @@ namespace CodicePlastico.TddSerie.Core.Tests
             cart.AddItem(99);
 
             Assert.Equal(2, cart.Items.ElementAt(0).Quantity);
+        }
+		
+		[Fact]
+        public void GetTotal_OneItem_ShouldAskToPriceListServiceTheItemPrice()
+        {
+			Moq<IPriceListService> priceListService = new Moq<IPriceListService>();
+            var cart = new ShoppingCart(priceListService.Object);
+            
+			cart.AddItem(99);
+            
+			priceListService.Verify(p => p.GetCurrentPriceFor(99));
+        }
+		
+		[Fact]
+        public void GetTotal_TwoDifferentItems_ShouldReturnTheSum()
+        {
+			Moq<IPriceListService> priceListService = new Moq<IPriceListService>();
+			priceListService.Setup(p => p.GetCurrentPriceFor(99)).Returns(100m);
+			priceListService.Setup(p => p.GetCurrentPriceFor(98)).Returns(50m);
+            
+			var cart = new ShoppingCart(priceListService.Object);
+            
+			cart.AddItem(99);
+			cart.AddItem(98);
+            
+			Assert.True(150m, cart.Total);
         }
     }
 }
