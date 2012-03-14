4af4fa2851831d49a3381903f6860483276170bf
ema <emanuele@codiceplastico.com>
2012-03-14 17:18:21 +0100

    completata storia

--- src/CodicePlastico.TddSerie.Core.Tests/ShoppingCartTests.cs
+++ src/CodicePlastico.TddSerie.Core.Tests/ShoppingCartTests.cs
@@ -6,65 +6,79 @@ namespace CodicePlastico.TddSerie.Core.Tests
 {
     public class ShoppingCartTests
     {
+        private Mock<IPriceListService> _priceListService;
+        private ShoppingCart _cart;
+
+        public ShoppingCartTests()
+        {
+            _priceListService = new Mock<IPriceListService>();
+            _cart = new ShoppingCart(_priceListService.Object);
+        }
+
         [Fact]
         public void AddItem_TheItemIsNotYetInTheBasket_ShouldAddItToTheBasket()
         {
-            var priceListService = new Mock<IPriceListService>();
-            var cart = new ShoppingCart(priceListService.Object);
+            _cart.AddItem(99);
 
-            cart.AddItem(99);
-
-            Assert.Equal(1, cart.ItemCount);
+            Assert.Equal(1, _cart.ItemCount);
         }
 
         [Fact]
         public void AddSameItemTwice_TheItemCountShouldBe1()
         {
-            var priceListService = new Mock<IPriceListService>();
-            var cart = new ShoppingCart(priceListService.Object);
+            _cart.AddItem(99);
+            _cart.AddItem(99);
 
-            cart.AddItem(99);
-            cart.AddItem(99);
-
-            Assert.Equal(1, cart.ItemCount);
+            Assert.Equal(1, _cart.ItemCount);
         }
 
         [Fact]
         public void AddSameItemTwice_TheItemQuantityShouldBe2()
         {
-            var priceListService = new Mock<IPriceListService>();
-            var cart = new ShoppingCart(priceListService.Object);
+            _cart.AddItem(99);
+            _cart.AddItem(99);
 
-            cart.AddItem(99);
-            cart.AddItem(99);
-
-            Assert.Equal(2, cart.Items.ElementAt(0).Quantity);
+            Assert.Equal(2, _cart.Items.ElementAt(0).Quantity);
         }
 		
 		[Fact]
         public void GetTotal_OneItem_ShouldAskToPriceListServiceTheItemPrice()
         {
-			var priceListService = new Mock<IPriceListService>();
-            var cart = new ShoppingCart(priceListService.Object);
-            
-			cart.AddItem(99);
-            
-			priceListService.Verify(p => p.GetCurrentPriceFor(99));
+			_cart.AddItem(99);
+			_priceListService.Verify(p => p.GetCurrentPriceFor(99));
+        }
+
+        [Fact]
+        public void GetTotal_OneItem_ShouldSetTheTotalValue()
+        {
+            _priceListService.Setup(p => p.GetCurrentPriceFor(99)).Returns(100m);
+            _cart.AddItem(99);
+
+            Assert.Equal(100m, _cart.Total);
         }
 		
+
 		[Fact]
         public void GetTotal_TwoDifferentItems_ShouldReturnTheSum()
         {
-			var priceListService = new Mock<IPriceListService>();
-			priceListService.Setup(p => p.GetCurrentPriceFor(99)).Returns(100m);
-			priceListService.Setup(p => p.GetCurrentPriceFor(98)).Returns(50m);
+			_priceListService.Setup(p => p.GetCurrentPriceFor(99)).Returns(100m);
+			_priceListService.Setup(p => p.GetCurrentPriceFor(98)).Returns(50m);
             
-			var cart = new ShoppingCart(priceListService.Object);
+			_cart.AddItem(99);
+			_cart.AddItem(98);
             
-			cart.AddItem(99);
-			cart.AddItem(98);
+			Assert.Equal(150m, _cart.Total);
+        }
+
+        [Fact]
+        public void GetTotal_SameItemTwice_ShouldReturnTheSum()
+        {
+            _priceListService.Setup(p => p.GetCurrentPriceFor(99)).Returns(100m);
             
-			Assert.Equal(150m, cart.Total);
+            _cart.AddItem(99);
+            _cart.AddItem(99);
+
+            Assert.Equal(200m, _cart.Total);
         }
     }
 }
